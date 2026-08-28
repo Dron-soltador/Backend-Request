259f158afeddf55bca4a92eafcdd230dfb975066
MerceMaron <mercedes.maron.711@gmail.com>
2026-08-28 12:50:40 -0300

    feat(pedidos): implementar endpoint de cotizacion de envios con distancia haversine

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,30 @@
 import os
 from flask import Flask
 from flask_cors import CORS
-from flasgger import Swagger  # <--- 1. Importamos Flasgger
+from flasgger import Swagger
 from app.models import db
 from app.auth import auth_bp
+from app.pedidos import pedidos_bp
+
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
 
-    # Configuración e inicialización de Swagger
     app.config['SWAGGER'] = {
-        'title': 'API de Pedidos - Autenticación',
+        'title': 'API de Pedidos - Autenticación y Cotización',
         'uiversion': 3
     }
-    Swagger(app)  # <--- 2. Le decimos a Flask que levante la interfaz de Swagger
+    Swagger(app)
 
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
+
+    # Asegúrate de que ambas líneas tengan exactamente 4 espacios de sangría
     app.register_blueprint(auth_bp)
+    app.register_blueprint(pedidos_bp)
 
     with app.app_context():
         db.create_all()
